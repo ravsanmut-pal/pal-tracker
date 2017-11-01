308b3b130968fd954f68d31f49b04c70a8937aea
ravsanmut-pal <ravsanmut.pal@gmail.com>
2017-11-01 12:54:47 +0530

    Completed Spring boot on CF

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,33 @@
 package io.pivotal.pal.tracker;
 
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController{
 
+
+    String hello = "hello";
+
+    public WelcomeController( @Value("${WELCOME_MESSAGE}") String message) {
+
+       this.hello = message;
+    }
+
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+
+        return this.hello;
     }
+
+
 }
+
+
+
+
