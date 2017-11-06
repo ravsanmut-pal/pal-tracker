0a8dc2e4f237d22c05d14b058aeb80d31f3618ea
ravsanmut-pal <ravsanmut.pal@gmail.com>
2017-11-06 12:43:20 +0530

    Updated to reflect JDBC

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,24 +2,38 @@ package io.pivotal.pal.tracker;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication{
 
 public  static void main(String args[]){
-
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     SpringApplication.run(PalTrackerApplication.class, args);
 }
 
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+
+        MysqlDataSource dataSource  = new MysqlDataSource();
+
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        return new JdbcTimeEntryRepository(dataSource);
+
     }
+
+
 
     @Bean
     public ObjectMapper jsonObjectMapper() {
